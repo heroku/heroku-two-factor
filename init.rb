d823329f278438b7fa446e5a635e0b5c9cef0cd6
Pedro Belo <pedro@heroku.com>
2013-02-13 14:48:14 -0800

    enhance flow so enabling two factor will change your api key
    no need to do login again

--- init.rb
+++ init.rb
@@ -17,12 +17,23 @@ module Heroku::Command
       File.open(js_code_file, "w") { |f| f.puts "var code = '#{url}';" }
       Launchy.open("#{support_path}/qrcode.html")
 
-      display "Please add this OTP to your favorite application and enter it below."
-      display "Keep in mind that two-factor will cause your API key to change, and expire every 30 days!"
-      print "Security code: "
-      code = ask
+      display "WARN: this will your API key to change, and expire every 30 days!"
+      display "Please add this OTP to your favorite application and login below."
 
-      heroku.two_factor_enable(code)
+      # ask for credentials again, this time storing the password in memory
+      Heroku::Auth.credentials = Heroku::Auth.ask_for_credentials
+
+      # make the actual API call to enable two factor
+      heroku.two_factor_enable(Heroku::Auth.two_factor_code)
+
+      # get a new api key using the password and two factor
+      new_api_key = Heroku::Auth.api_key(
+        Heroku::Auth.user, Heroku::Auth.current_session_password)
+
+      # store new api key to disk
+      Heroku::Auth.credentials = [Heroku::Auth.user, new_api_key]
+      Heroku::Auth.write_credentials
+
       display "Enabled two-factor authentication."
     ensure
       File.delete(js_code_file) rescue Errno::ENOENT
@@ -70,6 +81,8 @@ class Heroku::Auth
       params
     end
 
+    # redefine ask_for_credentials to also ask for the 2fa code,
+    # AND to store the password so we can reuse it later
     def ask_for_credentials
       puts "Enter your Heroku credentials."
 
@@ -77,12 +90,25 @@ class Heroku::Auth
       user = ask
 
       print "Password (typing will be hidden): "
-      password = running_on_windows? ? ask_for_password_on_windows : ask_for_password
+      @current_session_password = running_on_windows? ? ask_for_password_on_windows : ask_for_password
 
       print "Two-factor code (leave blank if none): "
       @code = ask
+      @code = nil if @code == ""
 
-      [user, api_key(user, password)]
+      [user, api_key(user, @current_session_password)]
+    end
+
+    def current_session_password
+      @current_session_password
+    end
+
+    def two_factor_code
+      @code
+    end
+
+    # do not touch ssh keys!
+    def associate_or_generate_ssh_key
     end
   end
 end
